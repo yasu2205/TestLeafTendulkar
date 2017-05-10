e937a95dc2d338e6b1494a6980f0b7f41b88df71
YaswanthYasu <yasu2205@gmail.com>
2017-05-10 19:13:16 +0530

    added yash1.java

---
+++ yash1.java
@@ -0,0 +1,10 @@
+ Volume in drive C is BOOTCAMP
+ Volume Serial Number is 78E9-F866
+
+ Directory of C:\Users\apple\Desktop\2205
+
+10-05-2017  19:10    <DIR>          .
+10-05-2017  19:10    <DIR>          ..
+10-05-2017  19:10                 0 yash1.java
+               1 File(s)              0 bytes
+               2 Dir(s)  125,477,302,272 bytes free
